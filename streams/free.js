64550a072cac52bd1463f5651cb95bd6e3dafa0e
TomFirth <tom.firth84@gmail.com>
2021-11-15 22:53:24 +0000

    free games update

--- streams/free.js
+++ streams/free.js
@@ -1,7 +1,5 @@
 const { MessageEmbed } = require('discord.js')
-const cron = require('cron')
-const https = require("https")
-const parseString = require("xml2js").parseString
+const parse = require('feed-reader').parse
 const firebase = require('firebase-admin')
 const config = require('../config')
 
@@ -10,39 +8,30 @@ const db = firebase.firestore()
 class FreeFeed {
   static start(client) {
     const freeGameDocId = 'agEt3DFhkDVt0O71Nf7x'
-    let scheduledMessage = new cron.CronJob(`00 00 * * * *`, async () => {
-      https.get(config.streams.free, res => {
-        const chunks = []
-        res
-          .on("data", data => chunks.push(data))
-          .on("end", () => {
-            const buffer = Buffer.concat(chunks)
-            const rss = buffer.toString()
-            parseString(rss, async (err, result) => {
-              const query = await db.collection('freeGame').doc(freeGameDocId).get()
-              const doc = result.rss.channel[0].item[0]
-              if(query.data().pubDate !== doc.pubDate[0]
-              && query.data().title !== doc.title[0]) {
-                let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.freeGames)
-                const description = doc.description[0].replace(/<.*>/, '')
-                const freeGame = new MessageEmbed()
-                  .setTitle(doc.title[0])
-                  .setURL(doc.link[0])
-                  .setDescription(description)
-                  .setColor('BLUE')
-                channel.send({ embeds: [freeGame] })
-                db.collection('freeGame').doc(freeGameDocId).update({
-                  description: description,
-                  link: doc.link[0],
-                  pubDate: doc.pubDate[0],
-                  title: doc.title[0]
-                })
-              }
-            })
-          })
-      })
-    })
-    scheduledMessage.start()
+    
+    parse(config.streams.news).then(result => {
+      const query = await db.collection('freeGame').doc(freeGameDocId).get()
+      const doc = result.rss.channel[0].item[0]
+      if(query.data().pubDate !== doc.pubDate[0]
+      && query.data().title !== doc.title[0]) {
+        let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.freeGames)
+        const description = doc.description[0].replace(/<.*>/, '')
+        const freeGame = new MessageEmbed()
+          .setTitle(doc.title[0])
+          .setURL(doc.link[0])
+          .setDescription(description)
+          .setColor('BLUE')
+        channel.send({ embeds: [freeGame] })
+        db.collection('freeGame').doc(freeGameDocId).update({
+          description: description,
+          link: doc.link[0],
+          pubDate: doc.pubDate[0],
+          title: doc.title[0]
+        })
+      }
+    }).catch(error => {
+      console.log(error)
+    }).finally(() => {})
   }
 }
 
