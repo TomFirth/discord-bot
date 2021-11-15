b4e0471dfd1b97cf05c5fecff320a47992ddd3d2
TomFirth <tom.firth84@gmail.com>
2021-11-15 23:28:43 +0000

    adding cron to news and games

--- streams/free.js
+++ streams/free.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js')
+const cron = require('cron')
 const parse = require('feed-reader').parse
 const firebase = require('firebase-admin')
 const config = require('../config')
@@ -7,30 +8,33 @@ const db = firebase.firestore()
 
 class FreeFeed {
   static start(client) {
-    const freeGameDocId = 'agEt3DFhkDVt0O71Nf7x'
-    parse(config.streams.free).then(async result => {
-      const query = await db.collection('freeGame').doc(freeGameDocId).get()
-      const doc = result.entries[0]
-      if(query.data().pubDate !== doc.publishedDate
-      && query.data().title !== doc.title) {
-        const description = doc.contentSnippet.replace(/<.*>/, '')
-        const freeGame = new MessageEmbed()
-          .setTitle(doc.title)
-          .setURL(doc.link)
-          .setDescription(description)
-          .setColor('BLUE')
-        let channel = await client.channels.cache.find(channel => channel.name === config.discord.channels.freeGames)
-        channel.send({ embeds: [freeGame] })
-        db.collection('freeGame').doc(freeGameDocId).update({
-          description: description,
-          link: doc.link,
-          pubDate: doc.publishedDate,
-          title: doc.title
-        })
-      }
-    }).catch(error => {
-      console.log(error)
-    }).finally(() => {})
+    let scheduledMessage = new cron.CronJob('00 * */1 * * *', () => {
+      const freeGameDocId = 'agEt3DFhkDVt0O71Nf7x'
+      parse(config.streams.free).then(async result => {
+        const query = await db.collection('freeGame').doc(freeGameDocId).get()
+        const doc = result.entries[0]
+        if(query.data().pubDate !== doc.publishedDate
+        && query.data().title !== doc.title) {
+          const description = doc.contentSnippet.replace(/<.*>/, '')
+          const freeGame = new MessageEmbed()
+            .setTitle(doc.title)
+            .setURL(doc.link)
+            .setDescription(description)
+            .setColor('BLUE')
+          let channel = await client.channels.cache.find(channel => channel.name === config.discord.channels.freeGames)
+          channel.send({ embeds: [freeGame] })
+          db.collection('freeGame').doc(freeGameDocId).update({
+            description: description,
+            link: doc.link,
+            pubDate: doc.publishedDate,
+            title: doc.title
+          })
+        }
+      }).catch(error => {
+        console.log(error)
+      }).finally(() => {})
+    })
+    scheduledMessage.start()
   }
 }
 
--- streams/news.js
+++ streams/news.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js')
+const cron = require('cron')
 const parse = require('feed-reader').parse
 const firebase = require('firebase-admin')
 const config = require('../config')
@@ -7,29 +8,32 @@ const db = firebase.firestore()
 
 class NewsFeed {
   static start(client) {
-    const newsDocId = 'tc64DI4XqbngplOEm3hO'
-    parse(config.streams.news).then(async result => {
-      const query = await db.collection('news').doc(newsDocId).get()
-      const doc = result.entries[0]
-      if(query.data().published !== doc.publishedDate
-      && query.data().title !== doc.title) {
-        const news = new MessageEmbed()
-          .setTitle(doc.title)
-          .setDescription(doc.contentSnippet)
-          .setURL(doc.link)
-          .setColor('RED')
-        let channel = await client.channels.cache.find(channel => channel.name === config.discord.channels.news)
-        channel.send({ embeds: [news] })
-        db.collection('news').doc(newsDocId).update({
-          description: doc.contentSnippet,
-          link: doc.link,
-          published: doc.publishedDate,
-          title: doc.title
-        })
-      }
-    }).catch(error => {
-      console.log(error)
-    }).finally(() => {})
+    let scheduledMessage = new cron.CronJob('00 */15 * * * *', () => {
+      const newsDocId = 'tc64DI4XqbngplOEm3hO'
+      parse(config.streams.news).then(async result => {
+        const query = await db.collection('news').doc(newsDocId).get()
+        const doc = result.entries[0]
+        if(query.data().published !== doc.publishedDate
+        && query.data().title !== doc.title) {
+          const news = new MessageEmbed()
+            .setTitle(doc.title)
+            .setDescription(doc.contentSnippet)
+            .setURL(doc.link)
+            .setColor('RED')
+          let channel = await client.channels.cache.find(channel => channel.name === config.discord.channels.news)
+          channel.send({ embeds: [news] })
+          db.collection('news').doc(newsDocId).update({
+            description: doc.contentSnippet,
+            link: doc.link,
+            published: doc.publishedDate,
+            title: doc.title
+          })
+        }
+      }).catch(error => {
+        console.log(error)
+      }).finally(() => {})
+    })
+    scheduledMessage.start()
   }
 }
 
