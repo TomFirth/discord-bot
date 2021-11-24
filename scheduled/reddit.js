3e6e903653d3e5e11e215bc8942cf4afbac35aa3
TomFirth <tom.firth84@gmail.com>
2021-11-24 12:41:35 +0000

    query logging

--- scheduled/reddit.js
+++ scheduled/reddit.js
@@ -16,8 +16,9 @@ class Reddit {
       })
       response.on('end', async () => {
         const query = await db.collection('reddit').doc(reddit.docId).get()
+        console.log("query", query)
         if(query.data().title !== releases.data.children.data.title
-          || query.data().title == undefined) {
+          || query.data() == undefined) {
           const releases = JSON.parse(str)
           const channel = client.channels.cache.find(channel => channel.name === reddit.destination)
           const post = new MessageEmbed()
