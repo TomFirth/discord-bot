829db62c746bab47f0f122db3bad58e504fdc98f
TomFirth <tom.firth84@gmail.com>
2021-11-24 12:51:53 +0000

    first in array

--- scheduled/reddit.js
+++ scheduled/reddit.js
@@ -17,17 +17,17 @@ class Reddit {
       response.on('end', async () => {
         const query = await db.collection('reddit').doc(reddit.docId).get()
         const releases = JSON.parse(str)
-        console.log("releases", releases.data.children)
-        if(query.data() == undefined || query.data().title !== releases.data.children.data.title) {
+        console.log("releases", releases.data.children[0].data)
+        if(query.data() == undefined || query.data().title !== releases.data.children[0].data.title) {
           const channel = client.channels.cache.find(channel => channel.name === reddit.destination)
           const post = new MessageEmbed()
             .setTitle(releases.data.children.data.title)
             .setAuthor(reddit.author)
-            .setImage(releases.data.children.data.preview.images[0].url)
+            .setImage(releases.data.children[0].data.preview.images[0].url)
             .setColor('#FF5700')
           channel.send({ embeds: [post] })
           db.collection('reddit').doc(reddit.docId).set({
-            title: releases.data.children.data.title
+            title: releases.data.children[0].data.title
           }, {merge: true})
         }
       })
