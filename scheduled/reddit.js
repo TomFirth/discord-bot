b476693d78a966b5d4056eaf1106762903a7e7e6
TomFirth <tom.firth84@gmail.com>
2021-11-24 12:30:16 +0000

    remove cron from reddit

--- scheduled/reddit.js
+++ scheduled/reddit.js
@@ -4,36 +4,33 @@ const { MessageEmbed } = require('discord.js')
 
 class Reddit {
   static start(client, reddit) {
-    let scheduledMessage = new cron.CronJob(reddit.scheduled, () => {
-      const url = new URL("https://www.reddit.com/r/" + reddit.subreddit + "/hot.json")
-      https.get({
-        hostname: url.hostname,
-        path: url.pathname,
-        headers: {'User-Agent': 'agent'}
-      }, async response => {
-        let str = ''
-        response.on('data', data => {
-          str += data
-        })
-        response.on('end', async () => {
-          const query = await db.collection('reddit').doc(reddit.docId).get()
-          if(query.data().title !== doc.title) {
-            const releases = JSON.parse(str)
-            const channel = client.channels.cache.find(channel => channel.name === reddit.destination)
-            const post = new MessageEmbed()
-              .setTitle(releases.data.children.data.title)
-              .setAuthor(reddit.author)
-              .setImage(releases.data.children.data.preview.images[0].url)
-              .setColor('#FF5700')
-            channel.send({ embeds: [post] })
-            db.collection('reddit').doc(reddit.docId).set({
-              title: releases.data.children.data.title
-            }, {merge: true})
-          }
-        })
+    const url = new URL("https://www.reddit.com/r/" + reddit.subreddit + "/hot.json")
+    https.get({
+      hostname: url.hostname,
+      path: url.pathname,
+      headers: {'User-Agent': 'agent'}
+    }, async response => {
+      let str = ''
+      response.on('data', data => {
+        str += data
+      })
+      response.on('end', async () => {
+        const query = await db.collection('reddit').doc(reddit.docId).get()
+        if(query.data().title !== doc.title) {
+          const releases = JSON.parse(str)
+          const channel = client.channels.cache.find(channel => channel.name === reddit.destination)
+          const post = new MessageEmbed()
+            .setTitle(releases.data.children.data.title)
+            .setAuthor(reddit.author)
+            .setImage(releases.data.children.data.preview.images[0].url)
+            .setColor('#FF5700')
+          channel.send({ embeds: [post] })
+          db.collection('reddit').doc(reddit.docId).set({
+            title: releases.data.children.data.title
+          }, {merge: true})
+        }
       })
     })
-    scheduledMessage.start()
   }
 }
 
