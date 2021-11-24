cc68ad78645e8830218b6ca8883f9ad02417c116
TomFirth <tom.firth84@gmail.com>
2021-11-24 14:31:13 +0000

    reedit final amends

--- scheduled/reddit.js
+++ scheduled/reddit.js
@@ -1,6 +1,5 @@
 const cron = require('cron')
 const https = require("https")
-const { MessageEmbed } = require('discord.js')
 
 class Reddit {
   static start(client, reddit, db) {
@@ -19,11 +18,9 @@ class Reddit {
         const releases = JSON.parse(str)
         const date = new Date()
         let hour = date.getHours()
-        console.log("doc title", query.data().title == releases.data.children[0].data.title)
-        console.log("images", releases.data.children[0].data)
         if(query.data() !== undefined
-        || releases.data.children[0].data.preview !== undefined
-        || query.data().title !== releases.data.children[0].data.title) {
+        && releases.data.children[0].data.url_overridden_by_dest !== undefined
+        && query.data().title !== releases.data.children[0].data.title) {
           // if (reddit.nsfw && (hour < 20 || hour !== 0)) return false
           const channel = client.channels.cache.find(channel => channel.name === reddit.destination)
           channel.send(releases.data.children[0].data.url_overridden_by_dest)
@@ -31,6 +28,10 @@ class Reddit {
             title: releases.data.children[0].data.title
           }, {merge: true})
         }
+      })
+      response.on('error',  (error) => {
+        const channel = client.channels.cache.find(channel => channel.name === "bot")
+        channel.send(error)
       })
     })
   }
