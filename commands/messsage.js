5ab1aa9c1f811d088749e3bb1d607b0f6df3cfcf
TomFirth <tom.firth84@gmail.com>
2021-11-25 15:02:24 +0000

    remove messages

--- commands/messsage.js
+++ commands/messsage.js
@@ -5,8 +5,8 @@ module.exports = {
   name: 'message',
   description: 'Send a custom message',
   execute(client, message) {
+    message.delete()
     if (message.member.id === config.discord.owner.id) {
-      message.delete()
       const args = message.content.split(" ")
       const destination = args[1]
       args.splice(0, 2)
