23284d5af0f56612e64132e6195c016479d9acc4
TomFirth <tom.firth84@gmail.com>
2021-11-25 00:30:14 +0000

    return you fool

--- commands/help.js
+++ commands/help.js
@@ -5,14 +5,14 @@ module.exports = {
   name: 'help',
   description: 'A list of my commands!',
   execute(client, message) {
+    const exclusions = ["help", "ping", "server", "uptime"]
     const help_embed = new MessageEmbed()
       .setTitle(`${client.user.username}'s Commands`)
       .setThumbnail(client.user.displayAvatarURL())
       .setColor('GREEN')
       .setDescription(client.botCommands.map(command => {
-        const exclusions = ["help", "ping", "server", "uptime"]
         if (!exclusions.includes(command.name)) {
-            `\`${command.emoji || '✔️'}\` \`.${command.name}\` - *${command.description || `No description available.`}*`
+          return `\`${command.emoji || '✔️'}\` \`.${command.name}\` - *${command.description || `No description available.`}*`
         }
       }).join(`\n`))
     return message.channel.send({ embeds: [help_embed] })
