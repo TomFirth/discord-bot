9ebb7c952d5d7b51b34c676599be7b3c47f6e2b9
TomFirth <tom.firth84@gmail.com>
2021-12-08 01:37:56 +0000

    remove threads

--- commands/games.js
+++ commands/games.js
@@ -15,22 +15,10 @@ module.exports = {
     cache.put("highlow", highLow)
     cache.put("highlownew", Math.floor(Math.random() * 12))
     cache.put("highlowstreak", 1)
-    message.channel.send(`**A new game has started!**\nThe first number is: **${highLow}**. Will the next number be HIGHER or LOWER?\n> (The range is 0-12)\n\`\`\`Play with "higher" or "lower"\`\`\``).then(ownMessage => {
-      ownMessage.channel.threads.create({
-        name: '🔢-higher-or-lower',
-        autoArchiveDuration: 60,
-        reason: 'ocd',
-      })
-    })
+    message.channel.send(`**A new game has started!**\nThe first number is: **${highLow}**. Will the next number be HIGHER or LOWER?\n> (The range is 0-12)\n\`\`\`Play with "higher" or "lower"\`\`\``)
   },
   guessTheNumber(message) {
     cache.put("guess", Math.floor(Math.random() * 999))
-    message.channel.send(`**A new game has started!**\n> (The range is 0-999 - 5 minute time limit!)\n\`\`\`Play with "guess <number>"\`\`\``).then(ownMessage => {
-      ownMessage.channel.threads.create({
-        name: '🔢-guess-the-number',
-        autoArchiveDuration: 60,
-        reason: 'ocd',
-      })
-    })
+    message.channel.send(`**A new game has started!**\n> (The range is 0-999 - 5 minute time limit!)\n\`\`\`Play with "guess <number>"\`\`\``)
   }
 }
