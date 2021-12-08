f06d3f819f3702db6224f9d44a66138491b0c83f
TomFirth <tom.firth84@gmail.com>
2021-12-08 01:33:51 +0000

    await removed

--- commands/games.js
+++ commands/games.js
@@ -16,7 +16,7 @@ module.exports = {
     cache.put("highlownew", Math.floor(Math.random() * 12))
     cache.put("highlowstreak", 1)
     message.channel.send(`**A new game has started!**\nThe first number is: **${highLow}**. Will the next number be HIGHER or LOWER?\n> (The range is 0-12)\n\`\`\`Play with "higher" or "lower"\`\`\``)
-    await message.channel.threads.create({
+    message.channel.threads.create({
       name: '🔢-higher-or-lower',
       autoArchiveDuration: 60,
       reason: 'ocd',
@@ -25,7 +25,7 @@ module.exports = {
   guessTheNumber(message) {
     cache.put("guess", Math.floor(Math.random() * 999))
     message.channel.send(`**A new game has started!**\n> (The range is 0-999 - 5 minute time limit!)\n\`\`\`Play with "guess <number>"\`\`\``)
-    await message.channel.threads.create({
+    message.channel.threads.create({
       name: '🔢-guess-the-number',
       autoArchiveDuration: 60,
       reason: 'ocd',
