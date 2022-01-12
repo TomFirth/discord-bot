76dc7ca9bb0e2f9c6d520787e876790856a22b7a
TomFirth <tom.firth84@gmail.com>
2022-01-12 01:05:20 +0000

    add lucky

---
+++ scheduled/lucky.js
@@ -0,0 +1,24 @@
+const cron = require("cron")
+const config = require("../config.json")
+
+class LuckyCron {
+  static start(client) {
+		let scheduledMessage = new cron.CronJob("00 00 00 * * 3", () => {
+			const guild = client.guilds.cache.get(config.discord.guildId)
+      const winner = Math.floor(Math.random() * guild.members.length)
+			guild.members.fetch()
+			.then(members => {
+				members.forEach((member, index) => {
+					if (member.user.username !== config.discord.owner.name && member._roles.includes("860466953582936094") && index == winner) {
+						const role = member.guild.roles.cache.find(role => role.name === "special")
+						member.roles.add(role)
+					}
+				})
+			})
+			.catch(error => console.error(error, message.channel))
+		})
+		scheduledMessage.start()
+	}
+}
+
+module.exports = LuckyCron
