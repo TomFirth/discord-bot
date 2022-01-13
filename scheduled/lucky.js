9a13e7107e4cb5c52080812bce9c610b31850d43
TomFirth <tom.firth84@gmail.com>
2022-01-13 13:05:13 +0000

    lucky change

--- scheduled/lucky.js
+++ scheduled/lucky.js
@@ -3,7 +3,7 @@ const config = require("../config.json")
 
 class LuckyCron {
   static start(client) {
-		let scheduledMessage = new cron.CronJob("00 00 13 * * *", () => {
+		let scheduledMessage = new cron.CronJob("00 00 12 * * 3", () => {
 			const guild = client.guilds.cache.get(config.discord.guildId)
 			guild.members.fetch()
 			.then(members => {
