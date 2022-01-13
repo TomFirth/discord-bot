a46eb2c0feca8dfa7ca75f2bd58722f075d46509
TomFirth <tom.firth84@gmail.com>
2022-01-13 12:27:27 +0000

    lucky change

--- scheduled/lucky.js
+++ scheduled/lucky.js
@@ -4,18 +4,16 @@ const config = require("../config.json")
 class LuckyCron {
   static start(client) {
 		let scheduledMessage = new cron.CronJob("00 */1 12 * * *", () => {
-			console.log("1")
 			const guild = client.guilds.cache.get(config.discord.guildId)
 			guild.members.fetch()
 			.then(members => {
-				const memberLength = members.size
+				const memberLength = guild.members.filter(member => !member.user.bot).size
 				const winner = Math.floor(Math.random() * memberLength)
-				console.log("2", members)
+				console.log("winner", memberLength)
 				let index = 0
 				members.forEach(member => {
-					console.log("3", index, member.user.username)
+					console.log(index, member.user.username)
 					if (member.user.username !== config.discord.owner.name && !member._roles.includes("860466953582936094") && index == winner) {
-						console.log("4")
 						const role = member.guild.roles.cache.find(role => role.name === "special")
 						member.roles.add(role)
 						let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.special)
