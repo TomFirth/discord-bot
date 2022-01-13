f4598a001041471008afb2b625e15efff0054b68
TomFirth <tom.firth84@gmail.com>
2022-01-13 11:49:41 +0000

    lucky changes

--- scheduled/lucky.js
+++ scheduled/lucky.js
@@ -3,23 +3,27 @@ const config = require("../config.json")
 
 class LuckyCron {
   static start(client) {
-		let scheduledMessage = new cron.CronJob("00 45 11 * * 4", () => {
-			const guild = client.guilds.cache.get(config.discord.guildId)
-      const winner = Math.floor(Math.random() * guild.members.length)
-			guild.members.fetch()
-			.then(members => {
-				members.forEach((member, index) => {
-					if (member.user.username !== config.discord.owner.name && member._roles.includes("860466953582936094") && index == winner) {
-						const role = member.guild.roles.cache.find(role => role.name === "special")
-						member.roles.add(role)
-						if (member.roles.cache.some(role => role.name !== "special")) {
-							let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.special)
-							channel.send(`Welcome ${member.user.username}`)
+		let scheduledMessage = new cron.CronJob("00 */1 11 * * 4", () => {
+			try {
+				const guild = client.guilds.cache.get(config.discord.guildId)
+				const winner = Math.floor(Math.random() * guild.members.length)
+				guild.members.fetch()
+				.then(members => {
+					members.forEach((member, index) => {
+						if (member.user.username !== config.discord.owner.name && member._roles.includes("860466953582936094") && index == winner) {
+							const role = member.guild.roles.cache.find(role => role.name === "special")
+							member.roles.add(role)
+							if (member.roles.cache.some(role => role.name !== "special")) {
+								let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.special)
+								channel.send(`Welcome ${member.user.username}`)
+							}
 						}
-					}
+					})
 				})
-			})
-			.catch(error => console.error(error, message.channel))
+				.catch(error => console.error(error))
+			} catch (error) {
+				console.error(error)
+			}
 		})
 		scheduledMessage.start()
 	}
