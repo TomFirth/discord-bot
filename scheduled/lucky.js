bce4c81c2e32c9de159f7596b9d5eebbf2c4ac3d
TomFirth <tom.firth84@gmail.com>
2022-01-13 12:01:15 +0000

    lucky changes

--- scheduled/lucky.js
+++ scheduled/lucky.js
@@ -3,27 +3,30 @@ const config = require("../config.json")
 
 class LuckyCron {
   static start(client) {
-		let scheduledMessage = new cron.CronJob("00 */1 11 * * 4", () => {
-			try {
-				const guild = client.guilds.cache.get(config.discord.guildId)
-				const winner = Math.floor(Math.random() * guild.members.length)
-				guild.members.fetch()
-				.then(members => {
-					members.forEach((member, index) => {
-						if (member.user.username !== config.discord.owner.name && member._roles.includes("860466953582936094") && index == winner) {
-							const role = member.guild.roles.cache.find(role => role.name === "special")
-							member.roles.add(role)
-							if (member.roles.cache.some(role => role.name !== "special")) {
-								let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.special)
-								channel.send(`Welcome ${member.user.username}`)
-							}
+		let scheduledMessage = new cron.CronJob("00 */1 12 * * *", () => {
+			console.log("1")
+			const guild = client.guilds.cache.get(config.discord.guildId)
+			console.log("2")
+			const winner = Math.floor(Math.random() * guild.members.length)
+			console.log("3")
+			guild.members.fetch()
+			.then(members => {
+				console.log("4")
+				members.forEach((member, index) => {
+					console.log("5")
+					if (member.user.username !== config.discord.owner.name && member._roles.includes("860466953582936094") && index == winner) {
+						console.log("6")
+						const role = member.guild.roles.cache.find(role => role.name === "special")
+						member.roles.add(role)
+						if (member.roles.cache.some(role => role.name !== "special")) {
+							console.log("7")
+							let channel = client.channels.cache.find(channel => channel.name === config.discord.channels.special)
+							channel.send(`Welcome ${member.user.username}`)
 						}
-					})
+					}
 				})
-				.catch(error => console.error(error))
-			} catch (error) {
-				console.error(error)
-			}
+			})
+			.catch(error => console.error(error))
 		})
 		scheduledMessage.start()
 	}
