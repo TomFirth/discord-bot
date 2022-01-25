81d2ebb3727b59d81460f382204ca06c0ac1953f
TomFirth <tom.firth84@gmail.com>
2022-01-25 14:42:30 +0000

    Reddit back to stream

--- streams/reddit.js
+++ streams/reddit.js
@@ -1,14 +1,21 @@
-const axios = require("axios")
+const https = require("https")
 const config = require("../config.json")
 
 class Reddit {
   static start(client, reddit, db) {
     const url = new URL("https://www.reddit.com/r/" + reddit.subreddit + "/top.json?t=" + reddit.frequency)
-    axios.get(url.hostname + url.pathname)
-    .then(response => {
+    https.get({
+      hostname: url.hostname,
+      path: url.pathname,
+      headers: {'User-Agent': 'agent'}
+    }, async stream => {
+      let str = ''
+      stream.on("data", data => {
+        str += data
+      })
       stream.on("end", async () => {
         const query = await db.collection("reddit").doc(reddit.docId).get()
-        const releases = JSON.parse(response.data)
+        const releases = JSON.parse(str)
         if(query.data() !== undefined
           && releases.data.children[0].data.url_overridden_by_dest !== undefined
           && query.data().title !== releases.data.children[0].data.title) {
