2656518fd6baf582912f23b70a94af1788a75ca6
TomFirth <tom.firth84@gmail.com>
2022-02-03 16:22:40 +0000

    add times for reddit

--- streams/reddit.js
+++ streams/reddit.js
@@ -14,24 +14,39 @@ class Reddit {
         str += data
       })
       stream.on("end", async () => {
-        const query = await db.collection("reddit").doc(reddit.docId).get()
-        const releases = JSON.parse(str)
-        if(query.data() !== undefined
-          && releases.data.children[0].data.url_overridden_by_dest !== undefined
-          && query.data().title !== releases.data.children[0].data.title) {
-          try {
-            let destination = await client.channels.cache.find(channel => channel.name === reddit.destination)
-            destination.send(`${reddit.nsfw} ${releases.data.children[0].data.url_overridden_by_dest} ${reddit.nsfw}`).then(ownMessage => {
-              if (reddit.poll) {
-                ownMessage.react(config.discord.emojis.thumbsUp)
-                ownMessage.react(config.discord.emojis.thumbsDown)
-              }
-            })
-            db.collection("reddit").doc(reddit.docId).set({
-              title: releases.data.children[0].data.title
-            }, {merge: true})
-          } catch(error) {
-            console.error("channel error", error, reddit)
+        // check safe for work hours
+        const startTime = '19:00:00';
+        const endTime = '23:59:00';
+        currentDate = new Date()   
+        startDate = new Date(currentDate.getTime())
+        startDate.setHours(startTime.split(":")[0])
+        endDate = new Date(currentDate.getTime())
+        endDate.setHours(endTime.split(":")[0])
+        endDate.setMinutes(endTime.split(":")[1])
+        const valid = startDate < currentDate && endDate > currentDate
+
+        if (nsfw && !valid) {
+          // Do nothing
+        } else {
+          const query = await db.collection("reddit").doc(reddit.docId).get()
+          const releases = JSON.parse(str)
+          if(query.data() !== undefined
+            && releases.data.children[0].data.url_overridden_by_dest !== undefined
+            && query.data().title !== releases.data.children[0].data.title) {
+            try {
+              let destination = await client.channels.cache.find(channel => channel.name === reddit.destination)
+              destination.send(`${reddit.nsfw} ${releases.data.children[0].data.url_overridden_by_dest} ${reddit.nsfw}`).then(ownMessage => {
+                if (reddit.poll) {
+                  ownMessage.react(config.discord.emojis.thumbsUp)
+                  ownMessage.react(config.discord.emojis.thumbsDown)
+                }
+              })
+              db.collection("reddit").doc(reddit.docId).set({
+                title: releases.data.children[0].data.title
+              }, {merge: true})
+            } catch(error) {
+              console.error("channel error", error, reddit)
+            }
           }
         }
       })
