2f471392b75220590f204503842f68db7a85ebd4
TomFirth <tom.firth84@gmail.com>
2022-02-10 00:00:40 +0000

    birthdays

--- scheduled/games.js
+++ scheduled/games.js
@@ -3,29 +3,31 @@ const cron = require("cron")
 const utilities = require("../scripts/utilities.js")
 const config = require("../config.json")
 
-function init(client, db, cache, game) {
-	let scheduledMessage = new cron.CronJob(game.frequency, async () => {
-		const query = await db.collection(game.db).where("used", "==", false).get()
-		let questions = []
-		query.forEach(doc => {
-			questions.push({
-				id: doc.id,
-				question: doc.data().question,
-				answer: doc.data().answer
+class Game {
+  static start(client, db, cache, game) {
+		let scheduledMessage = new cron.CronJob(game.frequency, async () => {
+			const query = await db.collection(game.db).where("used", "==", false).get()
+			let questions = []
+			query.forEach(doc => {
+				questions.push({
+					id: doc.id,
+					question: doc.data().question,
+					answer: doc.data().answer
+				})
 			})
+			if (questions.length < 5) {
+				utilities.channel(client, config.discord.channels.bot, `Less than 5 ${game.game} questions remaining.`)
+			}
+			const random = Math.floor(Math.random() * questions.length)
+			cache.put("answer", questions[random].answer)
+			db.collection(game.db).doc(questions[random].id).update({ used: true })
+			const gameEmbed = new MessageEmbed()
+				.setDescription(questions[random].question + `\nReply with: "answer <your answer>"`)
+				.setColor("GREEN")
+			utilities.channel(client, game.destination, { embeds: [gameEmbed] })
 		})
-		if (questions.length < 5) {
-			utilities.channel(client, config.discord.channels.bot, `Less than 5 ${game.game} questions remaining.`)
-		}
-		const random = Math.floor(Math.random() * questions.length)
-		cache.put("answer", questions[random].answer)
-		db.collection(game.db).doc(questions[random].id).update({ used: true })
-		const gameEmbed = new MessageEmbed()
-			.setDescription(questions[random].question + `\nReply with: "answer <your answer>"`)
-			.setColor("GREEN")
-		utilities.channel(client, game.destination, { embeds: [gameEmbed] })
-	})
-	scheduledMessage.start()
+		scheduledMessage.start()
+	}
 }
 
-module.exports = init
+module.exports = Game
