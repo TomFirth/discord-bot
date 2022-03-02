e830d1ddf473959abb42db8d7bb47dae0defae66
TomFirth <tom.firth84@gmail.com>
2022-03-02 13:15:31 +0000

    lucky amends

--- scheduled/lucky.js
+++ scheduled/lucky.js
@@ -3,13 +3,12 @@ const utilities = require("../scripts/utilities.js")
 const config = require("../config.json")
 
 function init(client) {
-	let scheduledMessage = new cron.CronJob("00 00 12 * * 3", () => {
-		const guild = client.guilds.cache.get(config.discord.guildId)
-		guild.members.fetch()
-		.then(members => {
+	let scheduledMessage = new cron.CronJob("00 00 14 * * 3", () => {
+		try {
+			const guild = client.guilds.cache.get(config.discord.guildId)
 			const memberLength = members.size
 			const winner = Math.floor(Math.random() * memberLength)
-			members.forEach(async (index, member) => {
+			guild.members.cache.forEach(async (index, member) => {
 				if (member.user.username !== config.discord.owner.name && !member._roles.includes("860466953582936094") && index == winner) {
 					const role = member.guild.roles.cache.find(role => role.name === "special")
 					member.roles.add(role)
@@ -17,8 +16,9 @@ function init(client) {
 					await utilities.channel(client, config.discord.channels.general, `${member.displayName} Is this week's lucky winner!`)
 				}
 			})
-		})
-		.catch(error => console.error(error))
+		} catch (error) {
+			console.error(error)
+		}
 	})
 	scheduledMessage.start()
 }
