9b39e83b9029d4f70898d31941af2ac8d89d36c7
TomFirth <tom.firth84@gmail.com>
2022-08-23 00:40:46 +0100

    rss send amend

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ fs.readdir("./events/", (error, files) => {
 // STREAMS
 config.rss.forEach(feed => {
   let scheduledMessage = new cron.CronJob("00 */1 * * * *", async () => {
+    console.log("rss started")
     await rss.start(client, feed, db)
   })
   scheduledMessage.start()
