0b6fcc0b8c1aafe497639c07e9709fb8d3762ebc
TomFirth <tom.firth84@gmail.com>
2022-08-31 19:19:25 +0100

    jokes api

--- scheduled/jokes.js
+++ scheduled/jokes.js
@@ -1,29 +1,25 @@
 const { EmbedBuilder } = require("discord.js")
+const axios = require('axios')
 const cron = require("cron")
 const utilities = require("../scripts/utilities")
 const config = require("../config.json")
 
 function init(client) {
 	let scheduledMessage = new cron.CronJob("00 00 10 * * 0", async () => {
-		const url = new URL("https://icanhazdadjoke.com/api")
-		https.get({
-			hostname: url.hostname,
-			path: url.pathname,
-			headers: {'User-Agent': 'agent'}
-		}, async stream => {
-			let str = ''
-			stream.on("data", data => {
-				str += data
+		try {
+			const response = await axios.get("https://icanhazdadjoke.com/", {
+				headers: {
+					Accept: "application/json",
+					"User-Agent": "axios 0.27.2"
+				}
 			})
-			stream.on("end", async () => {
-				const jokes = JSON.parse(str)
-				console.log()
-				const embed = new EmbedBuilder()
-					.setDescription(jokes.joke)
-					.setColor(utilities.randomColour())
-				utilities.channel(client, config.discord.channels.general, { embeds: [embed] })
-			})
-		})
+			const embed = new EmbedBuilder()
+				.setDescription(response.data.joke)
+				.setColor(utilities.randomColour())
+			utilities.channel(client, config.discord.channels.general, { embeds: [embed] })
+		} catch (error) {
+			console.error(error.toJSON())
+		}
 	})
 	scheduledMessage.start()
 }
