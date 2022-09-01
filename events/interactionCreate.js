ac77b65839523a1edd45737925e48c66fda21585
TomFirth <tom.firth84@gmail.com>
2022-09-01 16:43:09 +0100

    slash commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,6 @@
 const config = require("../config.json")
 
-module.exports = (client, interaction) => {
+module.exports = async (client, interaction) => {
 	if (!interaction.isCommand()) return
   const { commandName, options } = interaction
   const command = client.commands.get(commandName)
