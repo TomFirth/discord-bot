1ac5f72f18c7fd5e5c669020ddb5582e426fee40
TomFirth <tom.firth84@gmail.com>
2022-09-02 17:14:54 +0100

    channel fix

--- commands/channel.js
+++ commands/channel.js
@@ -1,3 +1,4 @@
+const { channelType } = require("discord.js")
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const config = require("../config.json")
 
@@ -16,27 +17,34 @@ module.exports = {
         ))
     .addStringOption(option =>
       option
-      .setName("name")
-      .setDescription("Channel name")
-      .setRequired(false)
-    ),
+        .setName("name")
+        .setDescription("Channel name")
+        .setRequired(false)
+    )
+    .addNumberOptions(option =>
+      option
+        .setName("limit")
+        .setDescription("Set the user limit for the channel")
+        .setRequired(false)),
   async execute(interaction) {
     // set user limit
     // set private/public
     let channelName = `${config.discord.emojis.clock} ${interaction.options.getString("name")}`
     if (interaction.options.getString("name")) channelName = `${config.discord.emojis.clock} ${interaction.options.getString("name")}`
 
-    let type = "GUILD_TEXT"
+    let type = channelType.GuildText
     let parent = config.discord.categories.tempText
+    let userLimit = interaction.options.getNumber("limit") || 2
 
     if (interaction.options.getString("type") == "channel_voice") {
-      type = "GUILD_VOICE"
+      type = channelType.GuildVoice
       parent = config.discord.categories.tempVoice
     }
     await interaction.guild.channels.create({
       name: channelName,
       type,
       parent,
+      userLimit,
       permissionOverwrites: [
       {
           id: interaction.guild.roles.everyone
