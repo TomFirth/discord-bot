b8e711dbd8d43dfdc9cdd61c655f76b0f03e2e92
TomFirth <tom.firth84@gmail.com>
2022-09-07 20:04:41 +0100

    pokemon fix?

--- scheduled/pokemon.js
+++ scheduled/pokemon.js
@@ -11,15 +11,15 @@ function pad (n) {
 }
 
 function init(client, db, cache) {
-	let scheduledMessage = new cron.CronJob("00 04 20 * * 3", () => {
+	let scheduledMessage = new cron.CronJob("00 05 20 * * 3", () => {
     const number = pad(Math.floor(Math.random() * 151))
     const pokeEmbed = new EmbedBuilder()
       .setTitle("Who's that Pokemon?")
       .setDescription(`Reply with: "answer <your answer>`)
       .setImage(`attachment://${number}.jpg`)
-    const pokemon = kanto[number + 1]
+    const pokemon = kanto.kanto[number + 1]
     console.log("kanto", kanto)
-    console.log("kanto", kanto[number + 1])
+    console.log("kanto", kanto.kanto[number + 1])
     cache.set("answer", pokemon)
     utilities.channel(client, config.discord.channels.general, { embeds: [pokeEmbed], files: [`../images/pokemon/questions/${number}.jpg`] })
     db.collection("answer").doc("uLLtQDVl1lo41har8LqO").update({
