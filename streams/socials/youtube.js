0cba17c9ba7977583290dcf71a0424da5aea9465
TomFirth <tom.firth84@gmail.com>
2022-09-11 22:18:40 +0100

    youtube added

---
+++ streams/socials/youtube.js
@@ -0,0 +1,35 @@
+const { EmbedBuilder } = require("discord.js")
+const Parser = require("rss-parser")
+const parser = new Parser()
+const utilities = require("../scripts/utilities")
+const config = require("../../config.json")
+
+class YoutubeFeed {
+  static async start(client, db, user) {
+    const query = await db.collection("youtube").doc(user.docId).get()
+    parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${user.id}`)
+    const item = feeds.items[0]
+    if (query.data().publishedDate !== item.pubDate
+      || query.data().title !== item.title) {
+      let description = ""
+      if (item.content || item.contentSnippet) {
+        description = item.content || item.contentSnippet || item.description || ""
+        description.replace(/<\/?[^>]+(>|$)/g, "")
+      }
+      let feedEmbed
+      feedEmbed = new EmbedBuilder()
+        .setColor(utilities.randomColour())
+        .setTitle(item.title)
+        .setURL(item.link)
+        .setTimestamp()
+      let channel = await client.channels.cache.find(channel => channel.name === config.discord.channels.socials)
+      channel.send({ embeds: [feedEmbed] })
+      db.collection("youtube").doc(user.docId).set({
+        publishedDate: item.pubDate,
+        title: item.title
+      }, {merge: true})
+    }
+  }
+}
+
+module.exports = YoutubeFeed
