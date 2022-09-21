22bdddece76f739728b758ba229b91a061016edf
TomFirth <tom.firth84@gmail.com>
2022-09-21 19:29:08 +0100

    pokemon check

--- scheduled/pokemon.js
+++ scheduled/pokemon.js
@@ -12,13 +12,13 @@ function pad (n) {
 
 function init(client, db, cache) {
 	let scheduledMessage = new cron.CronJob("00 00 19 * * 3", () => {
-    const random = Math.floor(Math.random() * kanto.length)
+    const random = Math.floor(Math.random() * kanto.length + 1)
     const number = pad(random)
     const pokeEmbed = new EmbedBuilder()
       .setTitle("Who's that Pokemon?")
       .setDescription(`Reply with: "answer <your answer>`)
       .setImage(`attachment://${number}.jpg`)
-    const pokemon = kanto[random]
+    const pokemon = kanto[random + 1]
     cache.set("answer", pokemon.toString())
     utilities.channel(client, config.discord.channels.general, { embeds: [pokeEmbed], files: [`../discord-bot/images/pokemon/questions/${number}.jpg`] })
     db.collection("answer").doc("uLLtQDVl1lo41har8LqO").update({
