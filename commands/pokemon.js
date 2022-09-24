630864c065576f5c0050c723571f6e166b8b2314
TomFirth <tom.firth84@gmail.com>
2022-09-24 12:29:43 +0100

    answer change

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -10,9 +10,9 @@ module.exports = {
     .setDescription("Who's that pokemon"),
   async execute(interaction) {
     const random = Math.floor(Math.random() * kanto.length)
-    console.log(kanto.length)
     var len = 3 - ('' + random).length
     number = (len > 0 ? new Array(++len).join('0') : '') + random
+    console.log("number", random, number)
     const pokeEmbed = new EmbedBuilder()
       .setTitle("Who's that Pokemon?")
       .setDescription(`Reply with: "/answer <your answer>`)
