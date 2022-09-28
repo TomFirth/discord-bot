ec720e88be7dcd896639400389d9c3826092df99
TomFirth <tom.firth84@gmail.com>
2022-09-28 18:11:49 +0100

    troll amends

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -19,23 +19,23 @@ module.exports = async (client, message) => {
 
   // SPECIFIC USER TROLLS
   trolls.forEach(troll => {
-    if (troll.includes && !troll.emoji) {
+    if (react) {
+      if (message.content.toLowerCase().includes(troll.message.toLowerCase()) && Math.floor(Math.random() * troll.chance) == 0) {
+        message.react(troll.react)
+      }
+    } else if (troll.includes && !troll.emoji && troll.response) {
       if (message.content.toLowerCase().includes(troll.message.toLowerCase()) && Math.floor(Math.random() * troll.chance) == 0) {
         message.channel.send(troll.response)
       }
-    } else if (troll.includes && troll.emoji) {
+    } else if (troll.includes && troll.emoji && troll.response) {
       if (message.content.toLowerCase().includes(troll.message.toLowerCase()) && Math.floor(Math.random() * troll.chance) == 0) {
         message.channel.send(troll.response).then(ownMessage => {
           ownMessage.react(troll.emoji)
         })
       }
-    } else if (!troll.includes && !troll.emoji) {
+    } else if (!troll.includes && !troll.emoji && troll.response) {
       if (message.content.toLowerCase() == troll.message.toLowerCase() && Math.floor(Math.random() * troll.chance) == 0) {
         message.channel.send(troll.response)
-      }
-    } else if (react) {
-      if (message.content.toLowerCase().includes(troll.message.toLowerCase()) && Math.floor(Math.random() * troll.chance) == 0) {
-        message.react(troll.react)
       }
     }
   })
