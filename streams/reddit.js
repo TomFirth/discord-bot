d248dc64933aae28e39a9455140c35fba0bc5d61
TomFirth <tom.firth84@gmail.com>
2022-10-01 23:38:00 +0100

    reddit private

--- streams/reddit.js
+++ streams/reddit.js
@@ -29,26 +29,33 @@ class Reddit {
         } else {
           const query = await db.collection("reddit").doc(reddit.docId).get()
           const releases = JSON.parse(str)
-          if (query.data() !== undefined
-            && releases.data.children[0].data.url_overridden_by_dest !== undefined
-            && query.data().title !== releases.data.children[0].data.title) {
-              try {
-                await client.channels.fetch(reddit.destination)
-                .then(channel => {
-                  channel.send(`${reddit.nsfw} ${releases.data.children[0].data.url_overridden_by_dest} ${reddit.nsfw}`)
-                  .then(ownMessage => {
-                    if (reddit.poll) {
-                      ownMessage.react(config.discord.emojis.thumbsUp)
-                      ownMessage.react(config.discord.emojis.thumbsDown)
-                    }
-                  })
-                })
-                db.collection("reddit").doc(reddit.docId).set({
-                  title: releases.data.children[0].data.title
-                }, {merge: true})
-              } catch (error) {
-                console.error(error)
+          if (releases.reason) console.error("subreddit is private")
+          else {
+            try {
+              if (query.data() !== undefined
+                && releases.data.children[0].data.url_overridden_by_dest !== undefined
+                && query.data().title !== releases.data.children[0].data.title) {
+                  try {
+                    await client.channels.fetch(reddit.destination)
+                    .then(channel => {
+                      channel.send(`${reddit.nsfw} ${releases.data.children[0].data.url_overridden_by_dest} ${reddit.nsfw}`)
+                      .then(ownMessage => {
+                        if (reddit.poll) {
+                          ownMessage.react(config.discord.emojis.thumbsUp)
+                          ownMessage.react(config.discord.emojis.thumbsDown)
+                        }
+                      })
+                    })
+                    db.collection("reddit").doc(reddit.docId).set({
+                      title: releases.data.children[0].data.title
+                    }, {merge: true})
+                  } catch (error) {
+                    console.error(error)
+                  }
               }
+            } catch (error) {
+              console.error(error)
+            }
           }
         }
       })
