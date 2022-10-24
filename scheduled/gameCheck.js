4ab0a734ee82c3d3e75d597efd5f5001f755c0ca
TomFirth <tom.firth84@gmail.com>
2022-10-24 16:15:49 +0100

    answer close, gameCheck, riddles wont be too long

--- scheduled/gameCheck.js
+++ scheduled/gameCheck.js
@@ -2,30 +2,28 @@ const cron = require("cron")
 const utilities = require("../scripts/utilities")
 const config = require("../config.json")
 
-class GameCheck {
-  static async start(client, db) {
-    const games = [
-      "",
-      "quiz",
-      "pokemon",
-      "",
-      "riddle",
-      "movies",
-      ""
-    ]
-    const today = new Date()
-    const dow = today.getDay()
-		let scheduledMessage = new cron.CronJob("0 0 0 * * *", async () => {
-      if (games[dow] !== "") {
-        const lastQuestion = db.collection("answer").doc("uLLtQDVl1lo41har8LqO")
-        const doc = await lastQuestion.get()
-        if (!doc.data().used) {
-          utilities.channel(client, config.discord.channels.general, doc.data().answer)
-        }
+async function init(client, db) {
+  const games = [
+    "",
+    "quiz",
+    "pokemon",
+    "",
+    "riddle",
+    "movies",
+    ""
+  ]
+  const today = new Date()
+  const dow = today.getDay()
+  let scheduledMessage = new cron.CronJob("0 0 0 * * *", async () => {
+    if (games[dow] !== "") {
+      const lastQuestion = db.collection("answer").doc("uLLtQDVl1lo41har8LqO")
+      const doc = await lastQuestion.get()
+      if (!doc.data().used) {
+        utilities.channel(client, config.discord.channels.general, doc.data().answer)
       }
-		})
-		scheduledMessage.start()
-	}
+    }
+  })
+  scheduledMessage.start()
 }
 
-module.exports = GameCheck
+module.exports = init
