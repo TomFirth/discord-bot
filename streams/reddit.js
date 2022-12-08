6274d7f0c56ff576916b602a981d238b50460bb4
TomFirth <tom.firth84@gmail.com>
2022-12-08 12:44:57 +0000

    reddit typo

--- streams/reddit.js
+++ streams/reddit.js
@@ -28,7 +28,7 @@ class Reddit {
           // Do nothing
         } else {
           const query = await db.collection("reddit").doc(reddit.docId).get()
-          if (data.chatAt(0) == "<") return
+          if (data.charAt(0) == "<") return
           const releases = JSON.parse(Buffer.concat(data).toString())
           if (releases.reason) console.error("subreddit is private")
           else {
