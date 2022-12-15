815afa01aeaeec2f2bcb5cdc36d9f6520e0dd580
TomFirth <tom.firth84@gmail.com>
2022-12-15 09:37:11 +0000

    ready status

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const config = require("../config.json")
 
 module.exports = (client) => {
 	const today  = new Date()
-	client.user.setActivity("Try SlashCommands");
+	client.user.setActivity("SlashCommands", { type: 'LISTENING' });
 	console.log(`${today.toLocaleString()} - ${client.user.tag} is logged on.`)
 }
