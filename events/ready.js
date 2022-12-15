d7bcbdbfddcd52679565a768b38fba7346044234
TomFirth <tom.firth84@gmail.com>
2022-12-15 09:35:44 +0000

    error and warning updates

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,5 @@ const config = require("../config.json")
 module.exports = (client) => {
 	const today  = new Date()
 	client.user.setActivity("Try SlashCommands");
-	console.log(`${client.users.size} users. ${client.channels.size} channels.`)
 	console.log(`${today.toLocaleString()} - ${client.user.tag} is logged on.`)
 }
